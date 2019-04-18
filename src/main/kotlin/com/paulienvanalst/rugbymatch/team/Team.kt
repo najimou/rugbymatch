2a92baa808bfda9d7d51b94d871d4c5f179af681
Najim AKCHA <nakcha@egencia.com>
2019-04-18 13:19:49 +0200

    Exo 1

--- src/main/kotlin/com/paulienvanalst/rugbymatch/team/Team.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/team/Team.kt
@@ -6,26 +6,29 @@ data class Team (val players: List<Player>, val name: TeamName) {
      * A team has enough players when it has at least 15 players
      */
     val hasEnoughPlayers : Boolean
-        get() = throw NotImplementedException()
+        get() = players.count() >= 15
 
     /**
      * A team has substitutes when it has more than 15 players
      */
     val hasAnySubstitutes : Boolean
-        get() = throw NotImplementedException()
+        get() = players.any { !it.isStarting }
 
     /**
      * A team has enough starting players when there are at least 15 players
      * wearing back numbers 1 until 15
      */
     val hasEnoughStartingPlayers : Boolean
-        get() = throw NotImplementedException()
+        get() = players.count { it.isStarting } >= 15
+
+    val scrumhalf : Player?
+        get() = players.find { it.position==Position.SCRUM_HALF }
 
     /**
      * The captain, when present, should always where back number 7
      */
     fun captainBackNumber(): Int? {
-        throw NotImplementedException()
+        return this.scrumhalf!!.backNumber
     }
 
     /**
@@ -33,7 +36,7 @@ data class Team (val players: List<Player>, val name: TeamName) {
      * the captain is wearing the first back number we can find among the starting players.
      */
     fun replacingCaptainBackNumber(): Int? {
-        throw NotImplementedException()
+        return this.scrumhalf?.backNumber ?: players.first{it.isStarting}.backNumber
     }
 
 
